88e22a1d096914a6b99fb5f8485c53a147331d21
Levi Redlin <48256408+lredlin@users.noreply.github.com>
2020-10-28 23:19:38 -0500

    Updates for sound functioning and 6.1.0

--- javascript experiment code/passive_comprehension_trial.js
+++ javascript experiment code/passive_comprehension_trial.js
@@ -1,38 +1,80 @@
-// JavaScript Document
-function passive_comprehension_trial(image, sound) {
-  var passive_comprehension_trial = {
-    timeline: [{
-        type: 'html-keyboard-response',
-        stimulus: '+',
-        choices: jsPsych.NO_KEYS,
-        trial_duration: 500
-      },
-      {
-        type: 'image-keyboard-response',
 
-        stimulus: jsPsych.timelineVariable('img'),
-        choices: jsPsych.NO_KEYS,
-        trial_duration: 2500
-      },
-      {
-        type: 'image-keyboard-response',
+// Plays audio file 
+async function playAudio(audio) {
+	audio.play()
+}
 
-        stimulus: 'blank.PNG',
-        choices: jsPsych.NO_KEYS,
-        trial_duration: 500
-      },
-      {
-        type: 'image-keyboard-response',
-        prompt: '<p>I am text associated with an image<\p>',
-        stimulus: jsPsych.timelineVariable('img'),
-        choices: jsPsych.NO_KEYS,
-        trial_duration: 2000
-      }
+// Sets timer then calls audio play function
+function audioAfterTime(audio, time) {
+	return new Promise(resolve => {
+		setTimeout(() => {
+			playAudio(audio);
+		}, time);
+	});
+}
 
-    ],
-    timeline_variables: [{
-      img: image
-    }]
-  }
-  return passive_comprehension_trial
+// Returns the passive comprehension trial timeline
+function passive_comprehension_trial(image, sound, prompt) {
+
+	// Sets audio instance
+	var audio = new Audio(sound);
+
+	// Adds the duration of the audio to the image displays within the timeline
+	audio.addEventListener("loadeddata", function() {
+		passive_comprehension_trial['timeline'][2]['trial_duration'] += 1000 * this.duration
+		passive_comprehension_trial['timeline'][5]['trial_duration'] += 1000 * this.duration
+	});
+
+	// Saves current folder in server for ease of path determination
+	var loc = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname + window.location.search
+
+	// Timeline object that will be returned
+	var passive_comprehension_trial = {
+		timeline: [{
+			// Displays fixation cross
+			type: 'html-keyboard-response',
+			stimulus: '+',
+			choices: jsPsych.NO_KEYS,
+			trial_duration: 500
+		}, {
+			// Calls sound function so that it will play during image display
+			type: 'call-function',
+			async: false,
+			func: function() { audioAfterTime(audio, 1000) }
+		},
+		{
+			// Displays image with no user response
+			type: 'image-keyboard-response',
+			stimulus: jsPsych.timelineVariable('img'),
+			choices: jsPsych.NO_KEYS,
+			trial_duration: 2500
+		},
+		{
+			// Blank screen in between displays
+			type: 'image-keyboard-response',
+			stimulus: loc + '/lcnl javascript experiments/blank.png',
+			choices: jsPsych.NO_KEYS,
+			trial_duration: 500
+		}, {
+			// Calls sound function so that it will play during image display
+			type: 'call-function',
+			async: false,
+			func: function() { audioAfterTime(audio, 1000) }
+		},
+		{
+			// Displays image a second time
+			type: 'image-keyboard-response',
+			prompt: '<p>'+prompt+'<\p>',
+			stimulus: jsPsych.timelineVariable('img'),
+			choices: jsPsych.NO_KEYS,
+			trial_duration: 2000
+		}
+		
+		],
+		timeline_variables: [{
+			img: image
+		}]
+	}
+	console.log(jsPsych.data.get());
+	return passive_comprehension_trial
 }
